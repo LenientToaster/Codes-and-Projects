5252081caa7430642ac2451c839b08cd49b5c54d
LenientToaster <20cjevans15@gmail.com>
2022-06-11 11:32:21 +0100

    Fifth Commit

--- main.js
+++ main.js
@@ -1,65 +1,28 @@
-function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+let min = 100;
+let max = 999;
+
+// let firstNumber = Math.floor((Math.random() * 100) + 1);
+// let secondNumber = Math.floor((Math.random() * 100) + 1);
+
+function getRandomNumberBetween(min,max){
+  return Math.floor(Math.random()*(max-min+1)+min);
 }
 
-$(document).ready(function () {
-  function handleClick() {
-    const things = $(".thing");
-    // things.toggleClass("--now-it-is-red");
-  }
+£(document).ready(function() {
+  $("#1stnum").html(getRandomNumberBetween);
+  $("#2ndnum").html(getRandomNumberBetween);
+})  
 
-  let twoDigit = getRandomNumber(10, 99);
-  const twoDigitNumberOutput = $("#number1a");
-  twoDigitNumberOutput.html(twoDigit);
 
-  let threeDigit = getRandomNumber(100, 999);
-  const threeDigitNumberOutput = $("#number1b");
-  threeDigitNumberOutput.html(threeDigit);
-
-  // let threeDigitNumber = getRandomNumber(100, 999);
-  // const threeDigitNumberOutput = $("#number1b");
-  // threeDigitNumberOutput.html(threeDigitNumber);
-
-  // let threeDigitNumber = getRandomNumber(100, 999);
-  // const threeDigitNumberOutput = $("#number1b");
-  // threeDigitNumberOutput.html(threeDigitNumber);
-
-  // let operatorSelector = ("#add","#subtract","#multiply","#divide");
-  // const operatorSelectorOutput = $("#operator1");
-  // operatorSelectorOutput.html(operatorSelector);
-
-$("button","#add").on("click",function() {
-  $("operator1").html("<p> + </p>");
-  })
-})
-
-  // switch $("#operator1") {
-  //   case $("button.add").click():
-  //     $("#operator1").html("+");
-  //     break;
-
-  //     case $("button.subtract").click():
-  //     $("#operator1").html("-");
-  //     break;
-  // }
-
-  // console.log(operator)
-
-//   const toggleButton = $("#toggle-colour");
-//   toggleButton.on("click", handleClick);
-
-//   const specialThing = $("#special-thing");
-//   const output = $("#key-press");
-
-//   $(document).on("keydown", function (event) {
-//     output.html(event.key);
-//     switch (event.key) {
-//       case "a":
-//         console.log("YOU PRESSED THE a KEY");
-//         break;
-//       case "b":
-//         console.log("YOU PRESSED THE b KEY");
-//         break;
-//     }
-//   });
-// });
+// //   $(document).on("keydown", function (event) {
+// //     output.html(event.key);
+// //     switch (event.key) {
+// //       case "a":
+// //         console.log("YOU PRESSED THE a KEY");
+// //         break;
+// //       case "b":
+// //         console.log("YOU PRESSED THE b KEY");
+// //         break;
+// //     }
+// //   });
+// // });
